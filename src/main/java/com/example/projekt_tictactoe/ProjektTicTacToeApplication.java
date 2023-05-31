2c8854bbad6d80b9776b0d5909e1d50b3f14100e
Jack Grzegorz Rospara <jackoofrost@gmail.com>
2023-05-31 15:20:08 +0200

    Tic-Tac-Toe

---
+++ src/main/java/com/example/projekt_tictactoe/ProjektTicTacToeApplication.java
@@ -0,0 +1,13 @@
+package com.example.projekt_tictactoe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjektTicTacToeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjektTicTacToeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/projekt_tictactoe/ProjektTicTacToeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projekt_tictactoe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjektTicTacToeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
